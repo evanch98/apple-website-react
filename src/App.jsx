22ef0f7a6f306771c39149c85e6ff85b504d227d
Kyaw Thu <evanch98@gmail.com>
2024-07-11 16:39:47 +0800

    adding the Features component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Highlights from './components/Highlights';
 import Model from './components/Model';
+import Features from './components/Features';
 
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
-      <Highlights />
+      <Features />
       <Model />
     </main>
   );
